2909735ab85f33117b28687595192fe31d8008bf
lizabet07 <lizaloveballet@gmail.com>
2026-03-12 22:24:47 +0300

    Update _init_.py

--- src/lab0111/_init_.py
+++ src/lab0111/_init_.py
@@ -1,9 +1,1 @@
-# __init__.py
-"""
-Модуль для лабораторной работы №1
-Класс BankAccount - банковский счет с полной функциональностью
-"""
 
-from model import BankAccount, AccountStatus, Transaction
-
-__all__ = ['BankAccount', 'AccountStatus', 'Transaction']
